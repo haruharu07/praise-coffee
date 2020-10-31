d510e375e1b0d23769537e8ba86f23ee9e770243
Haru Nakama <seburusu10.3@gmail.com>
2020-10-31 23:28:15 +0900

    second commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,7 +62,7 @@ module.exports = {
         {
             resolve: `gatsby-source-instagram`,
             options: {
-            username: `8499497966`,
+            username: process.env.INSTAGRAM_USER_NAME,
             maxPosts: 8,
             },
         },
